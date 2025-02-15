0bc7a8b7680b5ee35d39c147be09bc2c275cd8a3
swwebdev <swdevweb@gmail.com>
2025-02-14 18:43:54 -0700

    Add files via upload

---
+++ Project1/Project 1/js/gallery.js
@@ -0,0 +1,30 @@
+"use strict"
+
+//Array of Gallery Images
+const gallery_images = ["images/CatTrees.png","images/ThingGetsWashed.png","images/VolunteerWithTardigrade.png"]
+
+document.addEventListener("DOMContentLoaded", () => {
+  //Loads the image from the DOM
+  const displayImage = document.querySelectorAll("img")
+
+  //Left Button Event Listener
+  document.querySelector("#left_button").addEventListener("click", evt => {
+    //shifts array items to the left
+    let firstItem = gallery_images.shift();
+    gallery_images.push(firstItem);
+    //display images
+    for (let i = 0; i < displayImage.length; i++){
+      displayImage[i].src = gallery_images[i];
+    }
+  })
+  //Right Button Event Listener
+  document.querySelector("#right_button").addEventListener("click", evt => {
+    //shifts array items to the right
+    let lastItem = gallery_images.pop();
+    gallery_images.unshift(lastItem);
+    //display images
+    for (let i = 0; i < displayImage.length; i++){
+      displayImage[i].src = gallery_images[i];
+    }
+  })
+})
